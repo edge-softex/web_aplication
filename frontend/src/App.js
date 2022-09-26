8eeb349ae425e24b8a6867f413bb1077a5f675ff
bernardo1411 <bernardo1411@hotmail.com>
2022-09-26 19:55:26 -0300

    feat: add eslint to enforce standart  code formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,10 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Box from '@material-ui/core/Box';
-import TopBar from "./topbar/TopBar"
-import Dashboard from "./dashboard/Dashboard"
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: '#f0f2f5',
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  }
-}));
+import React from 'react';
 
 function App() {
-
-  const classes = useStyles();
-
   return (
-    <Box component="main" className={classes.root}>
-      <TopBar />
-      <Dashboard />
-    </Box>
+    <div>
+      <p>App</p>
+    </div>
   );
 }
 
