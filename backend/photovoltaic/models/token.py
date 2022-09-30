63b71bc13a42158187cb50206198d89e6a934cf0
Gustavo Costa <gustavo.melo@edge.ufal.br>
2022-09-30 12:26:53 -0300

    Documenting Utils

--- backend/photovoltaic/models/token.py
+++ backend/photovoltaic/models/token.py
@@ -5,5 +5,6 @@ from rest_framework.authtoken.models import Token
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """Create the token for the user"""
     if created:
         Token.objects.create(user=instance)
