c5fa3e51b98b3c269e16dad0696ccec89665ba69
bernardo1411 <bernardo1411@hotmail.com>
2022-10-06 17:01:12 -0300

    feat: creating card

--- frontend/src/components/Molecules/NavigationItems/Card/Card.js
+++ frontend/src/components/Molecules/NavigationItems/Card/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Card.css';
+
 function Card(props) {
   const { children } = props;
 
   return (
-    <div>
+    <div className="card_div-container">
       {children}
     </div>
   );
--- frontend/src/containers/Dashboard/Dashboard.js
+++ frontend/src/containers/Dashboard/Dashboard.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import Title from '../../components/atoms/Title/Title';
+import Card from '../../components/Molecules/NavigationItems/Card/Card';
 
 import './Dashboard.css';
 
 function Dashboard() {
   return (
     <div className="dashboard-div">
-      <Title>Dashboard</Title>
+      <div className="title_div-container">
+        <Title>Dashboard</Title>
+      </div>
+      <Card>
+        <h1>Teste</h1>
+      </Card>
     </div>
   );
 }
