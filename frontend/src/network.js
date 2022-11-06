2aa01bc25813fbc0104ff012c3487b29721265f6
Gustavo Costa <gustavo.melo@edge.ufal.br>
2022-11-06 18:24:48 -0300

    Fixing ports for GCP

--- frontend/src/network.js
+++ frontend/src/network.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8000/api';
+export const API_URL = process.env.PUBLIC_URL.replace('/%22', '').replace('%22', '');
 export const API_TOKEN = 'd6632565e2d7d9695f418cc3ab2e9eaf8392e9aa';
 // var username = 'softex';
 // var password = 'softexfotovoltaic123';
